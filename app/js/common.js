b6a17d09b4f0515e7e988a5ae8ab6da92dfdfd1b
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-04-22 03:48:17 +0300

    Start project

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,33 @@
-window.onload = function () {
-	
-	console.log('js is working!!!'); 
+$( document ).ready(function() {
 
-}
+	// ---------------------- #search-open --------------------------
 
-/* $( document ).ready(function() {
+	$('#search-open').on('click', function() {
+		$(this).toggleClass('active');
+		$('#search').fadeToggle();
+	});
 
-	console.log( 'JQuery!!!' );
+	// ---------------------- initialize sliders --------------------------
 
-}); */
+	$('.line__slider').owlCarousel({
+		loop:true,
+		margin:26,
+		items: 9,
+		responsive: {
+			0:{
+				items: 2
+			},
+			768:{
+				items: 4
+			},
+			992:{
+				items: 6
+			},
+			1200:{
+				items: 8
+			},
+
+		}
+	});
+
+});
